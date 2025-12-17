355178481fb85faa65c884a5cf4e4fdd02bc69cd
Samuel1505 <oluwakoredeakinola@gmail.com>
2025-12-17 19:57:01 +0100

    feat:import compinent into app

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,7 +6,7 @@ import Footer from './components/Footer';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-zinc-50 via-white to-zinc-100 dark:from-black dark:via-zinc-950 dark:to-zinc-900">
+    <div className="min-h-screen bg-linear-to-br from-zinc-50 via-white to-zinc-100 dark:from-black dark:via-zinc-950 dark:to-zinc-900">
       <Navbar />
       <Hero />
       <Features />
