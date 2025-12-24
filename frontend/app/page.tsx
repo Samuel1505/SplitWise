773ddcf9276e692a322782f9f7b638026f7a13ae
Samuel1505 <oluwakoredeakinola@gmail.com>
2025-12-24 10:22:43 +0100

    added all components across page to home

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,21 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
+import UseCases from './components/UseCases';
+import Statistics from './components/Statistics';
 import CTA from './components/CTA';
 import Footer from './components/Footer';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-linear-to-br from-zinc-50 via-white to-zinc-100 dark:from-black dark:via-zinc-950 dark:to-zinc-900">
+    <div className="min-h-screen bg-white dark:bg-zinc-950">
       <Navbar />
       <Hero />
+      <Statistics />
       <Features />
+      <HowItWorks />
+      <UseCases />
       <CTA />
       <Footer />
     </div>
